6c253923f3991605dc54cc1212848e87bc0f976f
pratik chindhe <169028142+TxPrtaik@users.noreply.github.com>
2025-09-27 13:19:58 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 let mysql=require("mysql")
 let util=require("util")
 let con=mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"chalanSoftware"
+    "host":"biuiowq04vtryrmrqkwz-mysql.services.clever-cloud.com",
+    "user":"uvrafei5oreyagdm",
+    "password":"bsx5flrCuvvhroLQDPSX",
+    "database":"biuiowq04vtryrmrqkwz"
 })
 module.exports=util.promisify(con.query).bind(con)
